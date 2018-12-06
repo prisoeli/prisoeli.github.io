829c394728acc33c183c04503bea40e098af5e4e
unknown <prisoeli000@warren.k12.in.us>
2018-12-06 13:45:02 -0500

    transfer

---
+++ brains.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(".centered").slideDown(2000);
+    
+        $(".workpage").click(function () {
+            $(".home").slideUp(1000);
+            $(".aboutme").slideUp(1000);
+            $(".contactme").slideUp(1000);
+            $(".work").slideDown(1000);
+        })
+
+        $(".homepage").click(function () {
+            $(".aboutme").slideUp(1000);
+            $(".contactme").slideUp(1000);
+            $(".work").slideUp(1000);
+            $(".home").slideDown(1000);
+            $(".centered").fadeIn(3000);
+        })
+
+        $(".aboutmepage").click(function () {
+            $(".home").slideUp(1000);
+            $(".contactme").slideUp(1000);
+            $(".work").slideUp(1000);
+            $(".aboutme").slideDown(1000);
+            $(".centered").fadeOut();
+        })
+
+        $(".contactmepage").click(function () {
+            $(".home").slideUp(1000);
+            $(".aboutme").slideUp(1000);
+            $(".work").slideUp(1000);
+            $(".contactme").slideDown(1000);
+            $(".centered").fadeOut();
+        })
+
+    }
+)
+
